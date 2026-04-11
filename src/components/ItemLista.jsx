1943f67fc22dbeb3e51e84a078f238e8fa6cffff
JaegerArtur <artur.jaeger13@gmail.com>
2026-04-11 17:07:45 -0300

    commit final

--- src/components/ItemLista.jsx
+++ src/components/ItemLista.jsx
@@ -1,13 +1,17 @@
+import "./ItemLista.css";
+
 export default function ItemLista({ item, acaoBotao, textoBotao, acaoRemover }) {
   return (
     <li>
-      {item}
-      <button type="button" onClick={acaoBotao}>
-        {textoBotao}
-      </button>
-      <button type="button" onClick={acaoRemover}>
-        Remover
-      </button>
+      <p>{item}</p>
+      <div className="botoes">
+        <button type="button" onClick={acaoBotao}>
+          {textoBotao}
+        </button>
+        <button type="button" onClick={acaoRemover}>
+          Remover
+        </button>
+      </div>
     </li>
   );
 }
